ca7168c6539a832191263b75fba3ec5e55c299bc
sajjad-alam-786 <shaikhsajjad786@gmail.com>
2025-07-22 17:51:53 +0530

    docs: update homepage heading text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <main className="px-10 mt-10">
-        <h1> This Is automatic deployement test Text</h1>
+        <h1> This Is automatic deployement test Text again</h1>
         <Hero />
       </main>
       {/* <Footer/> */}
