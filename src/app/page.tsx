0654c758a6cf646e135babef89264a9fff628a99
sajjad-alam-786 <shaikhsajjad786@gmail.com>
2025-07-22 17:39:53 +0530

    feat: add deployment test text to home page
    Add text to verify automatic deployment process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-
 import Hero from "@/app/components/hero";
 
 export default function Home() {
   return (
     <>
-      
       <main className="px-10 mt-10">
-        <Hero/>
+        <h1> This Is automatic deployement test Text</h1>
+        <Hero />
       </main>
       {/* <Footer/> */}
     </>
